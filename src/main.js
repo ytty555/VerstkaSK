38c0f5becfa7c855d21af1deee1c23dd182e03e2
Yuriy Titov <yurtitov@gmail.com>
2019-04-18 18:57:45 +0200

    temp

--- src/main.js
+++ src/main.js
@@ -1,33 +1,61 @@
 import React, { Component } from 'react';
 import './css/style.css';
 
+{/* <div className="page page_pos_left page_color page_delegated page_make-up page_photo"> */}
+function ElMakeUp(props) {
+  return (
+    <button className="page__el-make-up"></button>
+  );
+}
+
+function ElPhoto(props) {
+  let classN = 'page__el-photo';
+
+  if (props.value) {
+    classN += ' page_photo';
+  }
+
+  return (
+    <button className="page__el-photo"></button>
+  );
+}
+
+function ElDelegated(props) {
+  return (
+    <button className="page__el-delegated"></button>
+  );
+}
+
+function Page(props) {
+  let classN ='page ' + props.pos + ' ' + props.color;
+  return (
+    <div className={classN}>
+      <div className="page__el-background"></div>
+      <div className="page__el-color"></div>
+      <div className="page__el-number">{props.pageNum}</div>
+      <ElMakeUp value={props.makeup} />
+      <ElPhoto value={props.photo}/>
+      <ElDelegated value={props.delegated}/>
+      
+    </div>
+  );
+}
+
 function PagePair(props) {
-    return (
-      <div className="page-pair">
-        <img className="page-pair-background" src={require('./img/pair.svg')} alt="Изображение разворота полос" />
-        <div className="page page_pos_left page_color page_delegated page_make-up page_photo">
-          <div className="page__el-background"></div>
-          <div className="page__el-color"></div>
-          <div className="page__el-number">{props.leftPage}</div>
-          <button className="page__el-make-up"></button>
-          <button className="page__el-photo"></button>
-          <button className="page__el-delegated"></button>
-        </div>
-        <div className="page page_pos_right page_color page_delegated page_make-up page_photo">
-          <div className="page__el-background"></div>
-          <div className="page__el-color"></div>
-          <div className="page__el-number">{props.rightPage}</div>
-          <button className="page__el-make-up"></button>
-          <button className="page__el-photo"></button>
-          <button className="page__el-delegated"></button>
-        </div>
-      </div>
-    );
+  return (
+    <div className="page-pair">
+      <img className="page-pair-background" src={require('./img/pair.svg')} alt="Изображение разворота полос" />
+      <Page pageNum={props.leftPage} pos='page_pos_left' color='page_color' delegated='page_delegated' makeup='' photo={1} />
+      <Page pageNum={props.rightPage} pos='page_pos_right' color='' delegated='' makeup='page_make-up' />
+    </div>
+  );
 }
 class MainBlock extends Component {
   renderPagePair(leftP, rightP) {
-    return <PagePair leftPage={leftP} rightPage={rightP}/>
+    return <PagePair leftPage={leftP} rightPage={rightP} />
   }
+
+  
 
   render() {
     return (
@@ -36,7 +64,6 @@ class MainBlock extends Component {
           <h1 className="visually-hidden">Раскладка полос</h1>
           <div className="page-grid-container">
             {this.renderPagePair('02', '03')}
-       
           </div>
         </section>
       </main>
