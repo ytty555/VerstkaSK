8163fd5976ea417a419cd5694690e648424090da
Yuriy Titov <yurtitov@gmail.com>
2019-04-19 16:34:37 +0200

    temp

--- src/main.js
+++ src/main.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import './css/style.css';
 
 function ElMakeUp(props) {
-  let classN = 'page__el-make-up ' + props.value;
+  let classN = 'page__el-make-up ' + props.valueState;
   return (
     <button className={classN}></button>
   );
 }
 
 function ElPhoto(props) {
-  let classN = 'page__el-photo ' + props.value;
+  let classN = 'page__el-photo ' + props.valueState;
 
   return (
     <button className={classN}></button>
@@ -17,35 +17,36 @@ function ElPhoto(props) {
 }
 
 function ElDelegated(props) {
-  let classN = 'page__el-delegated ' + props.value;
+  let classN = 'page__el-delegated ' + props.valueState;
   return (
     <button className={classN}></button>
   );
 }
 
 function Page(props) {
+  let isMakeUp = props.pageState.pageState.makeup;
+  let isPhoto = props.pageState.pageState.photo;
+  let isDelegated = props.pageState.pageState.delegated;
+
   let classN = 'page ' + props.pos + ' ' + (props.pageState.pageColor ? 'page_color' : '');
   return (
     <div className={classN}>
       <div className="page__el-background"></div>
       <div className="page__el-color"></div>
       <div className="page__el-number">{props.pageState.pageID}</div>
-      <ElMakeUp value={props.pageState.makeup ? 'page_make-up' : ''} />
-      <ElPhoto value={props.pageState.photo ? 'page_photo' : ''} />
-      <ElDelegated value={props.pageState.delegated ? 'page_delegated' : ''} />
+      <ElMakeUp valueState={isMakeUp ? 'page_make-up' : ''} />
+      <ElPhoto valueState={isPhoto ? 'page_photo' : ''} />
+      <ElDelegated valueState={isDelegated ? 'page_delegated' : ''} />
     </div>
   );
 }
 
 function PagePair(props) {
-  let stateLeftPage = props.stateLeftPage;
-  let stateRightPage = props.stateRightPage;
-
   return (
     <div className="page-pair">
       <img className="page-pair-background" src={require('./img/pair.svg')} alt="Изображение разворота полос" />
-      <Page pos='page_pos_left' pageState={stateLeftPage} />
-      <Page pos='page_pos_right' pageState={stateRightPage} />
+      <Page pos='page_pos_left' pageState={props.stateLeftPage} />
+      <Page pos='page_pos_right' pageState={props.stateRightPage} />
     </div>
   );
 }
@@ -55,22 +56,23 @@ class MainBlock extends Component {
     let resArr = [];
     let pageInfoObj = {
       pageID: null,
-      pageColor: 0,
+      pageColor: null,
       pageState: {
-        makeup: 0,
-        photo: 0,
-        delegated: 0,
+        makeup: null,
+        photo: null,
+        delegated: null,
       },
     };
 
     for (let i = 1; i <= pageQuantity; i++) {
       pageInfoObj.pageID = i < 10 ? '0' + i : String(i);
-      pageInfoObj.pageColor = 0;
-      pageInfoObj.pageState.makeup = 0;
-      pageInfoObj.pageState.photo = 0;
-      pageInfoObj.pageState.delegated = 0;
-
-      resArr.push(pageInfoObj);
+      pageInfoObj.pageColor = 1;
+      pageInfoObj.pageState.makeup = 1;
+      pageInfoObj.pageState.photo = 1;
+      pageInfoObj.pageState.delegated = 1;
+      let tempObj = Object.assign({}, pageInfoObj);
+      
+      resArr.push(tempObj);
     }
 
     return resArr;
@@ -82,6 +84,7 @@ class MainBlock extends Component {
   }
 
   arrPagesPair(count) {
+    let currState = this.state;
     function numForPair(count, pair) {
       let res = [null, null];
 
@@ -101,7 +104,7 @@ class MainBlock extends Component {
 
     function renderPagePair(leftP, rightP) {
       function getCurrentPageStatus(pID) {
-        let allPageState = this.state;
+        let allPageState = currState;
         for (let i = 0; i < allPageState.length; i++) {
           if (allPageState[i].pageID === pID) {
             return allPageState[i];
@@ -112,7 +115,7 @@ class MainBlock extends Component {
 
       let lPS = getCurrentPageStatus(leftP);
       let rPS = getCurrentPageStatus(rightP);
-      return <PagePair leftPage={leftP} rightPage={rightP} stateLeftPage={lPS} stateRightPage={rPS} />
+      return <PagePair stateLeftPage={lPS} stateRightPage={rPS} />
     }
 
     let arr = [];
