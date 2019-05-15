100e62a34d35ec71c98483c1e0dae2593d5e95b8
Yuriy Titov <yurtitov@gmail.com>
2019-05-15 14:26:29 +0200

    temp

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import FooterMain from './FooterMain/FooterMain';
-import ControlPanel from './ControlPanel/ControlPanel';
 import MainBlock from './MainBlock/MainBlock';
 
 class App extends Component {
   render() {
-    return (
-      <div className="all-blocks">
-        <ControlPanel />
-        <MainBlock />
-        <FooterMain />
-      </div>
+    return ( <
+      div className = "all-blocks" >
+      <
+      MainBlock / >
+      <
+      FooterMain / >
+      <
+      /div>
     );
   }
 }
--- src/components/MainBlock/MainBlock.js
+++ src/components/MainBlock/MainBlock.js
@@ -1,4 +1,6 @@
-import React, { Component } from "react";
+import React, {
+  Component
+} from "react";
 import PagePair from "./PagePair";
 
 class MainBlock extends Component {
@@ -61,7 +63,7 @@ class MainBlock extends Component {
   };
 
   render() {
-    const info = this.getPageInfo(this.state);
+    const info = this.getPageInfo();
     const pQuantity = info.pagesQuantity;
     const pMakeUp = info.pagesMakeUp;
     const pPhoto = info.pagesPhoto;
@@ -71,17 +73,33 @@ class MainBlock extends Component {
       <React.Fragment>
         <header className="header-main">
           <p className="header__info">Всего полос в номере: <span>{pQuantity}</span></p>
-          <p className="header__info">Сверстано полос: <span>{pMakeUp}</span></p>
+          <p className="header__info">Сверстано полос: <span>{pMakeUp + pDelegated}</span></p>
+          <p className="header__info">Осталось сверстать: <span>{pQuantity - (pMakeUp + pDelegated)}</span></p>
         </header>
         <main className="main-block">
           <section className="page-section">
-            <h1 className="visually-hidden">Раскладка полос</h1>
-            <div className="page-grid-container">
-              {this.renderPagePairList(this.getPageQuantity())}
-            </div>
+            <h1 className="visually-hidden"> Раскладка полос </h1>
+            <div className="page-grid-container"> {this.renderPagePairList(this.getPageQuantity())}</div>
           </section>
         </main>
-      </React.Fragment>
+        <section className="control-panel">
+          <ul>
+            <li className="choose-pages">
+              <input name="pages" type="radio" className="choose-pages__radio" id="24" />
+              <label htmlFor="24">24 полос</label>
+            </li>
+            <li className="choose-pages">
+              <input name="pages" type="radio" className="choose-pages__radio" id="28" />
+              <label htmlFor="28">28 полос</label>
+            </li>
+            <li className="choose-pages">
+              <input name="pages" type="radio" className="choose-pages__radio" id="32" />
+              <label htmlFor="32">32 полос</label>
+            </li>
+          </ul>
+          <button>Создать раскладку</button>
+        </section>
+      </React.Fragment >
     );
   }
 
@@ -91,14 +109,23 @@ class MainBlock extends Component {
     let el = null;
 
     for (let i = 1; i <= PageQuantity / 2; i++) {
-      el = (
-        <PagePair
-          key={this.toTwo(i)}
-          statePage={state[this.toTwo(i)]}
-          handleOnClickMakeup={this.handleOnClickMakeUp(i)}
-          handleOnClickPhoto={this.handleOnClickPhoto(i)}
-          handleOnClickDelegated={this.handleOnClickDelegated(i)}
-        />
+      el = (<
+        PagePair key={
+          this.toTwo(i)
+        }
+        statePage={
+          state[this.toTwo(i)]
+        }
+        handleOnClickMakeup={
+          this.handleOnClickMakeUp(i)
+        }
+        handleOnClickPhoto={
+          this.handleOnClickPhoto(i)
+        }
+        handleOnClickDelegated={
+          this.handleOnClickDelegated(i)
+        }
+      />
       );
       elList.push(el);
     }
@@ -112,12 +139,12 @@ class MainBlock extends Component {
     const stateTmp = Object.assign({}, state);
     stateTmp[posLR].pageMakeup = !state[posLR].pageMakeup;
     stateTmp[posLR].pageDelegated =
-      stateTmp[posLR].pageDelegated && stateTmp[posLR].pageMakeup
-        ? false
-        : stateTmp[posLR].pageDelegated;
-    stateTmp[posLR].pagePhoto = !stateTmp[posLR].pageMakeup
-      ? false
-      : stateTmp[posLR].pagePhoto;
+      stateTmp[posLR].pageDelegated && stateTmp[posLR].pageMakeup ?
+        false :
+        stateTmp[posLR].pageDelegated;
+    stateTmp[posLR].pagePhoto = !stateTmp[posLR].pageMakeup ?
+      false :
+      stateTmp[posLR].pagePhoto;
     this.setState({
       [state]: stateTmp
     });
@@ -140,36 +167,36 @@ class MainBlock extends Component {
     const stateTmp = Object.assign({}, state);
     stateTmp[posLR].pageDelegated = !state[posLR].pageDelegated;
     stateTmp[posLR].pageMakeup =
-      stateTmp[posLR].pageMakeup && stateTmp[posLR].pageDelegated
-        ? false
-        : stateTmp[posLR].pageMakeup;
-    stateTmp[posLR].pagePhoto = !stateTmp[posLR].pageMakeup
-      ? false
-      : stateTmp[posLR].pagePhoto;
+      stateTmp[posLR].pageMakeup && stateTmp[posLR].pageDelegated ?
+        false :
+        stateTmp[posLR].pageMakeup;
+    stateTmp[posLR].pagePhoto = !stateTmp[posLR].pageMakeup ?
+      false :
+      stateTmp[posLR].pagePhoto;
     this.setState({
       [state]: stateTmp
     });
   };
 
   // получаем сводную информацию по полосам из state
-  getPageInfo = (stateObj) => {
+  getPageInfo = () => {
+    const currState = this.state;
     let pagesDelegated = 0;
     let pagesMakeUp = 0;
     let pagesPhoto = 0;
     let res = {};
+    console.log('State --', currState);
 
-    
-    // полос делегировано
-    for (let pair in stateObj) {
-      for (let page in stateObj[pair]) {
-        const currPage = stateObj[pair][page];
-        console.log(currPage);
+    for (let pair in currState) {
+      if (pair == "[object Object]") break;
+      for (let page in currState[pair]) {
+        const currPage = currState[pair][page];
         pagesDelegated += currPage.pageDelegated ? 1 : 0;
         pagesMakeUp += currPage.pageMakeup ? 1 : 0;
         pagesPhoto += currPage.pagePhoto ? 1 : 0;
       }
     }
-  
+
     res.pagesQuantity = this.getPageQuantity();
     res.pagesDelegated = pagesDelegated;
     res.pagesMakeUp = pagesMakeUp;
